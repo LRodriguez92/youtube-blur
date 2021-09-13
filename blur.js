c8e242b713b32450d1bb1017f55b0bca8d9d6058
Leonardo <leonardo.rodriguez92@gmail.com>
2021-09-13 17:35:49 -0400

    can toggle blur on and off

--- blur.js
+++ blur.js
@@ -1,20 +1,21 @@
-console.log("connected!")
-
-chrome.runtime.onMessage.addListener((request) => {
-    console.log("Request: ", request)
+chrome.runtime.onMessage.addListener((request) => {  
     if (request) {
         blurThumbnails()
         window.addEventListener("scroll", blurThumbnails)
-    } 
+    } else {
+        showThumbnails()
+        window.addEventListener("scroll", showThumbnails)
+        }
 })
-
+    
+    
 const blurThumbnails = () => {
     console.log("blurring!")
-    const images = document.querySelectorAll("#thumbnail, #avatar")
     
+    const images = document.querySelectorAll("#thumbnail, #avatar")
     const textBlocks = document.querySelectorAll(".yt-formatted-string, #video-title, .ytd-video-meta-block, .ytd-rich-shelf-renderer")
     const badges = document.querySelectorAll(".badge-style-type-live-now, .ytd-badge-supported-renderer")
-
+        
     images.forEach(image => {
         if(image.id === "avatar") {
             image.style.filter = "blur(5px)"
@@ -25,11 +26,37 @@ const blurThumbnails = () => {
 
     textBlocks.forEach(text => {
         text.style.color = "transparent"
-        text.style.textShadow = "0 0 8px #FFF"
+        text.style.textShadow = "0 0 8px #fff"
     })
 
     badges.forEach(badge => {
         badge.style.display = "none"
+    })
+}
+
+const showThumbnails = () => {
+    console.log("showing thumbnails!")
+    
+    const images = document.querySelectorAll("#thumbnail, #avatar")
+    const textBlocks = document.querySelectorAll(".yt-formatted-string, #video-title, .ytd-video-meta-block, .ytd-rich-shelf-renderer")
+    const badges = document.querySelectorAll(".badge-style-type-live-now, .ytd-badge-supported-renderer")
+
+
+    images.forEach(image => {
+        if(image.id === "avatar") {
+            image.style.filter = "blur(0)"
+        } else {
+            image.style.filter = "blur(0)"
+        }
+    })
+
+    textBlocks.forEach(text => {
+        text.style.color = "#fff"
+        text.style.textShadow = "none"
+    })
+
+    badges.forEach(badge => {
+        badge.style.display = "block"
     })
 }
 
