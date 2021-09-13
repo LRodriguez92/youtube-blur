f783d2c06ed000bb18257a203c94d0037a1cb6f8
Leonardo <leonardo.rodriguez92@gmail.com>
2021-09-13 14:37:19 -0400

    added functionality

---
+++ blur.js
@@ -0,0 +1,35 @@
+const blurSwitch = document.querySelector("#blur-input")
+
+const blurThumbnails = () => {
+    const images = document.querySelectorAll("#thumbnail, #avatar")
+    
+    const textBlocks = document.querySelectorAll(".yt-formatted-string, #video-title, .ytd-video-meta-block, .ytd-rich-shelf-renderer")
+    const badges = document.querySelectorAll(".badge-style-type-live-now, .ytd-badge-supported-renderer")
+
+    images.forEach(image => {
+        if(image.id === "avatar") {
+            image.style.filter = "blur(5px)"
+        } else {
+            image.style.filter = "blur(30px)"
+        }
+    })
+
+    textBlocks.forEach(text => {
+        text.style.color = "transparent"
+        text.style.textShadow = "0 0 8px #FFF"
+    })
+
+    badges.forEach(badge => {
+        badge.style.display = "none"
+    })
+}
+
+
+const blurChecked = () => {
+    if (blurSwitch.checked) {
+        window.onload(blurChecked)
+        window.addEventListener("scroll", blurThumbnails)
+    }
+}
+
+blurSwitch.addEventListener("click", blurChecked)
